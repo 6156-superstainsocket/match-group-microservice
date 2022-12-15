ee1908bc10d6c34abf5c82b64c26becf3fbb51be
adobemomo <741618856@qq.com>
2022-12-15 11:22:27 -0500

    remove description validate in group create

--- groups/serializers.py
+++ groups/serializers.py
@@ -29,7 +29,7 @@ class GroupSerializer(WritableNestedModelSerializer):
     class Meta:
         model = Group
         fields = ['id', 'name', 'description', 'icon_id', 'allow_without_approval', 'tags', 'admin_user_id']
-        read_only_fields = READ_ONLY_FIELDS + ('admin_user_id',)
+        read_only_fields = READ_ONLY_FIELDS + ('admin_user_id', 'description')
     
 
 class LikeSerializer(serializers.ModelSerializer):
